1b08bff9aaf928ce3215d2add560bd390366e306
Lorena Bezerra de Almeida <lore.lo2310@gmail.com>
2022-04-19 22:00:25 -0300

    Put the game loop in the main() funcion

--- main-terminal.py
+++ main-terminal.py
@@ -24,53 +24,55 @@ def check_winning(board, line_pos, col_pos):
             return False
     return True        
 
-# board = [
-#     [0,3,4,6,7,8,9,1,2],
-#     [6,7,2,1,9,5,3,4,8],
-#     [1,9,8,3,4,2,5,6,7],
-#     [8,5,9,7,6,1,4,2,3],
-#     [4,2,6,8,5,3,7,9,1],
-#     [7,1,3,9,2,4,8,5,6],
-#     [9,6,1,5,3,7,2,8,4],
-#     [2,8,7,4,1,9,6,3,5],
-#     [3,4,5,2,8,6,1,7,9],
-# ]
+def main():
+    # board = [
+    #     [5,3,4,6,7,8,9,1,2],
+    #     [6,7,2,1,9,5,3,4,8],
+    #     [1,9,8,3,4,2,5,6,7],
+    #     [8,5,9,7,6,1,4,2,3],
+    #     [4,2,6,8,5,3,7,9,1],
+    #     [7,1,3,9,2,4,8,5,6],
+    #     [9,6,1,5,3,7,2,8,4],
+    #     [2,8,7,4,1,9,6,3,5],
+    #     [3,4,5,2,8,6,1,7,9],
+    # ]
 
-rows = 9
-board = create_board(rows, rows)
-run = True
-won = True
-print_board(board)
-
-# Main game loop
-while run:
-    # Ask for player input
-    line = int(input("Enter a line: "))
-
-    if (line == 0):
-        run = False
-        continue
-
-    col = int(input("Enter a column: "))
-    num = int(input("Enter a number: "))
-
-    # Add the number to the board
-    board[line-1][col-1] = num
-
-    full = board_full(board)
-
+    rows = 9
+    board = create_board(rows, rows)
+    run = True
+    won = True
     print_board(board)
 
-    if full:
-        # Checks winning for every position in the board
-        for line in range(len(board)):
-            for col in range(len(board[0])):
-                if not(check_winning(board, line, col)):
-                    won = False
-                    break
-        if won:
-            print("WON")
-        else:
-            print("LOST")
-        break
+    # Main game loop
+    while run:
+        # Ask for player input
+        line = int(input("Enter a line: "))
+
+        if (line == 0):
+            run = False
+            continue
+
+        col = int(input("Enter a column: "))
+        num = int(input("Enter a number: "))
+
+        # Add the number to the board
+        board[line-1][col-1] = num
+
+        full = board_full(board)
+
+        print_board(board)
+
+        if full:
+            # Checks winning for every position in the board
+            for line in range(len(board)):
+                for col in range(len(board[0])):
+                    if not(check_winning(board, line, col)):
+                        won = False
+                        break
+            if won:
+                print("WON")
+            else:
+                print("LOST")
+            break
     
+main()
